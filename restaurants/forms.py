c542fada16b9c41bfd84d6b05df599d0087d9d7d
YzSheng <winsonyin123@gmail.com>
2017-03-09 20:36:19 +0800

    add the Ueditor in the comments

--- restaurants/forms.py
+++ restaurants/forms.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 from django import forms
+from DjangoUeditor.forms import UEditorField
 
 class CommentForm(forms.Form):
     visitor = forms.CharField(max_length=20)
     email = forms.EmailField(max_length=20, required=False, label='E-mail')
-    content = forms.CharField(max_length=200, widget=forms.Textarea())
+    content = UEditorField(u"评论内容",initial=u"很好吃",width=800,height=100)
+    # content = forms.CharField(max_length=200, widget=forms.Textarea())
 
     def clean_content(self):
         content = self.cleaned_data['content']
         if len(content) < 5:
-            raise forms.ValidationError('字数不足')
+            raise forms.ValidationError(u'字数不足')
         return content
